fd19f983c14f0bf1c1f45e005eeb82e05893d8c8
Ivan Saez Zamora <ivsaez@gmail.com>
2024-02-12 22:07:42 +0100

    update dependencies

--- Desiring/Desire.cs
+++ Desiring/Desire.cs
@@ -1,15 +1,25 @@
-﻿using Identification;
+﻿using Agents;
+using Identification;
+using Items;
+using Mapping;
+using Saver;
 using Worlding;
 
 namespace Desiring
 {
-    public delegate int Heuristic(IWorld world);
+    public delegate int Heuristic<A, I, M>(IWorld<A, I, M> world)
+        where A : IAgent, ITimed, ISavable, ICloneable
+        where I : IItem, ITimed, ISavable, ICloneable
+        where M : IMapped, ITimed, ISavable, ICloneable;
 
-    public class Desire : Identifiable
+    public class Desire<A, I, M> : Identifiable
+        where A : IAgent, ITimed, ISavable, ICloneable
+        where I : IItem, ITimed, ISavable, ICloneable
+        where M : IMapped, ITimed, ISavable, ICloneable
     {
-        public Heuristic Heuristic { get; }
+        public Heuristic<A, I, M> Heuristic { get; }
 
-        public Desire(string id, Heuristic heuristic)
+        public Desire(string id, Heuristic<A, I, M> heuristic)
             : base(id)
         {
             Heuristic = heuristic;
